9325b07d589525885f546431ffb5a24c2eb459f8
Micah Powell <micahpowell2024@gmail.com>
2025-02-06 10:07:29 -0500

    combined average putts and score function, and added function to find best and worst score or putts

--- GITLAB.py
+++ GITLAB.py
@@ -3,15 +3,30 @@ import pandas as pd
 
 stats = pd.read_csv("Golfstats.csv")
 
-def avgscore(score):
-    return (round(sum(score)/len(score),2))
+def average(x):
+    return (round(sum(x)/len(x),2))
 
-def avgputts(putt):
-    return sum(putt)/len(putt)
+def best(x):
+    return min(x)
+
+def worst(x):
+    return max(x)
 
 scores = stats["score"].tolist()
 putts = stats["putts"].tolist()
+
 roundsplayed = len(scores)
+
 par = 72
-averagescore = avgscore(scores)
+
+averagescore = average(scores)
 print (averagescore)
+
+averageputts = average(putts)
+print (averageputts)
+
+print(worst(scores))
+print(best(scores))
+print(worst(putts))
+print(best(putts))
+
